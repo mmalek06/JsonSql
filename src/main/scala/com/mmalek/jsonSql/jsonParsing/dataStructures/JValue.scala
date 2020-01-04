c0008c7123a10b2eaf22791a14e47c728fb0869b
mmalek06@gmail.com <mmalek06@gmail.com>
2020-01-04 19:06:15 +0100

    jvalue cleanup

--- src/main/scala/com/mmalek/jsonSql/jsonParsing/dataStructures/JValue.scala
+++ src/main/scala/com/mmalek/jsonSql/jsonParsing/dataStructures/JValue.scala
@@ -3,15 +3,7 @@ package com.mmalek.jsonSql.jsonParsing.dataStructures
 //taken from: https://github.com/lift/lift/blob/master/framework/lift-base/lift-json/src/main/scala/net/liftweb/json/JsonAST.scala
 sealed abstract class JValue {
   type Values
-
-  def apply(i: Int): JValue = JNothing
   def values: Values
-  def children: Seq[JValue] = this match {
-    case JObject(l) => l
-    case JArray(l) => l
-    case JField(_, v) => Seq(v)
-    case _ => Nil
-  }
 }
 
 //noinspection TypeAnnotation
@@ -50,7 +42,6 @@ case class JBool(value: Boolean) extends JValue {
 case class JField(name: String, value: JValue) extends JValue {
   override type Values = (String, value.Values)
   override def values = (name, value.values)
-  override def apply(i: Int): JValue = value(i)
 }
 
 //noinspection TypeAnnotation
@@ -68,7 +59,6 @@ case class JObject(obj: Seq[JField]) extends JValue {
 case class JArray(arr: Seq[JValue]) extends JValue {
   override type Values = Seq[Any]
   override def values = arr.map(_.values)
-  override def apply(i: Int): JValue = arr(i)
 
   override def equals(that: Any): Boolean = that match {
     case a: JArray => Set(arr.toArray: _*) == Set(a.arr.toArray: _*)
