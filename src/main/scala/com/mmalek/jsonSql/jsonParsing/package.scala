89e31d76d7f96c3570705104125edbf0746ee31b
mmalek06@gmail.com <mmalek06@gmail.com>
2020-01-04 18:08:37 +0100

    selecting data

---
+++ src/main/scala/com/mmalek/jsonSql/jsonParsing/package.scala
@@ -0,0 +1,132 @@
+package com.mmalek.jsonSql
+
+import com.mmalek.jsonSql.jsonParsing.Types.CreatorArgument
+import com.mmalek.jsonSql.jsonParsing.dataStructures._
+import com.mmalek.jsonSql.jsonParsing.fsm.State._
+import com.mmalek.jsonSql.jsonParsing.fsm.{State, StateMachine}
+import shapeless.Poly1
+
+import scala.language.postfixOps
+
+package object jsonParsing {
+  def getJson(input: String): JValue = {
+    val seed = getSeed
+    val ParsingTuple(_, tree, _, _, _) = Normalizer
+      .normalize(input)
+      .foldLeft(seed)((aggregate, char) => {
+        aggregate.stateMachine.next(char, aggregate.builder, aggregate.statesHistory).map(sm => {
+          val actionTuple = getActionTuple(aggregate.stateMachine.state, aggregate.builder)
+
+          aggregate.builder.clear()
+          aggregate.builder.append(char)
+
+          val nextHistory = sm.state :: aggregate.statesHistory.toList
+
+          createParsingTuple(sm, aggregate, nextHistory, actionTuple)
+        }).getOrElse({
+          aggregate.builder.append(char)
+          aggregate
+        })
+      })
+
+    tree.execute
+  }
+
+  private def getSeed =
+    ParsingTuple(
+      new StateMachine(State.Initial),
+      FunctionsTree.zero,
+      Nil,
+      new StringBuilder,
+      Nil)
+
+  private def getActionTuple(state: State, sb: StringBuilder) =
+    state match {
+      case ReadObjectEnd | ReadArrayEnd => (Navigation.Up, KeyNode(""), None)
+      case ReadObject => (Navigation.Down, ObjectNode, Some(getObject))
+      case ReadObjectKey => (Navigation.Down, KeyNode(sb.toString), Some(getPropertyKey(getCleanedPropertyKey(sb))))
+      case ReadArray => (Navigation.Down, ArrayNode, Some(getArray))
+      case ReadScalar => (Navigation.Up, ScalarNode(sb.toString), Some(getScalar(getCleanedScalar(sb))))
+      case _ => (Navigation.Stay, KeyNode(sb.toString), None)
+    }
+
+  private def createParsingTuple(sm: StateMachine,
+                                 aggregate: ParsingTuple,
+                                 history: Seq[State],
+                                 actionTuple: (Navigation, NodeKind, Option[CreatorArgument => JValue])) = {
+    val (navigation, childKind, f) = actionTuple
+    val newTree = f.map(aggregate.tree.addChild(childKind, _, aggregate.currentTreePath)).getOrElse(aggregate.tree)
+    val rightmostPath = newTree.getRightmostChildPath()
+    val oldPath = updatePathObjects(aggregate.currentTreePath, rightmostPath)
+    val path =
+      if (navigation == Navigation.Up) oldPath.init
+      else if (navigation == Navigation.Stay) oldPath
+      else rightmostPath
+
+    ParsingTuple(sm, newTree, path, aggregate.builder, history)
+  }
+
+  private def updatePathObjects(currentTreePath: Seq[Node], newObjects: Seq[Node]) =
+    currentTreePath
+      .foldLeft((List.empty[Node], newObjects))((pair, _) => pair match {
+        case (newPath, updater) => (newPath :+ updater.head, updater.tail) })
+      ._1
+
+  private def getCleanedPropertyKey(sb: StringBuilder) = {
+    val value = sb.toString
+    val firstQuoteIdx = value.indexOf('"')
+    val secondQuoteIdx = value.length - value.reverse.indexOf('"')
+
+    value.substring(firstQuoteIdx + 1, secondQuoteIdx - 1)
+  }
+
+  private def getCleanedScalar(sb: StringBuilder) = {
+    val value = sb.toString
+    val firstQuoteIdx = value.indexOf('"')
+    val secondQuoteIdx = value.length - value.reverse.indexOf('"')
+
+    if (firstQuoteIdx < 0) value.trim
+    else value.substring(firstQuoteIdx + 1, secondQuoteIdx - 1)
+  }
+
+  private val getPropertyKey = (name: String) =>
+    (arg: CreatorArgument) => JField(name, arg.fold(CreatorArgumentToJValue))
+
+  private val getScalar = (scalar: String) => (_: CreatorArgument) =>
+    if (scalar.forall(_.isDigit)) JInt(BigInt(scalar))
+    else scalar
+      .toDoubleOption
+      .map(d => JDouble(d))
+      .getOrElse(scalar
+        .toBooleanOption
+        .map(b => JBool(b))
+        .getOrElse(if (scalar == "null") JNull else JString(scalar)))
+
+  private val getObject = (arg: CreatorArgument) =>
+    arg.select[JObject] match {
+      case None => JNull
+      case Some(obj) => obj
+    }
+
+  private val getArray = (arg: CreatorArgument) =>
+    arg.select[JArray] match {
+      case None => JNull
+      case Some(array) => array
+    }
+
+  object CreatorArgumentToJValue extends Poly1 {
+    implicit val atJString: Case.Aux[JString, JValue] = at { x: JString => x}
+    implicit val atJDouble: Case.Aux[JDouble, JValue] = at { x: JDouble => x}
+    implicit val atJInt: Case.Aux[JInt, JValue] = at { x: JInt => x}
+    implicit val atJBool: Case.Aux[JBool, JValue] = at { x: JBool => x}
+    implicit val atFields: Case.Aux[JObject, JValue] = at { x: JObject => x }
+    implicit val atValues: Case.Aux[JArray, JValue] = at { x: JArray => x }
+    implicit val atUnit: Case.Aux[Unit, JValue] = at { _: Unit => JNull }
+  }
+
+  private case class ParsingTuple(stateMachine: StateMachine,
+                                  tree: FunctionsTree,
+                                  currentTreePath: Seq[Node],
+                                  builder: StringBuilder,
+                                  statesHistory: Seq[State])
+}
---
+++ src/main/scala/com/mmalek/jsonSql/package.scala
@@ -0,0 +1,75 @@
+package com.mmalek
+
+import com.mmalek.jsonSql.jsonParsing.dataStructures.{JArray, JObject, JValue}
+import com.mmalek.jsonSql.jsonParsing.getJson
+import com.mmalek.jsonSql.sqlParsing.Token.{From, Json, Select, Where, Any => AnyToken}
+import com.mmalek.jsonSql.sqlParsing.{Token, tokenize}
+
+package object jsonSql {
+  def runJsonSql(rawSql: String, rawJson: String): Option[Seq[Seq[Option[JValue]]]] = {
+    val json = getJson(rawJson)
+    val tokens = tokenize(rawSql)
+    val completeTokens = putJsonToken(tokens, json)
+    val actions = getActions(completeTokens)
+
+    actions.from().map(j => actions.select(j.value))
+  }
+
+  private def putJsonToken(tokens: Seq[Token], json: JValue) =
+    tokens.flatMap(t => if (t == From) List(t, Json(json)) else List(t))
+
+  private def getActions(completeTokens: Seq[Token]) = {
+    val actions = completeTokens.foldLeft(ActionsTuple(None, Map[Token, Seq[AnyToken]]()))(foldAsActionData).actions
+
+    new {
+      def select(json: JValue): Seq[Seq[Option[JValue]]] =
+        actions(Select).map(getValues(_, json))
+
+      def from(): Option[Json] =
+        actions(From).head match {
+          case token: Json => Some(token)
+          case _ => None
+        }
+
+      def where(json: JValue): Option[JValue] =
+        ???
+    }
+  }
+
+  private def foldAsActionData(aggregate: ActionsTuple, token: Token) = {
+    token match {
+      case Select => ActionsTuple(Some(Select), aggregate.actions ++ Map(Select -> Nil))
+      case From => ActionsTuple(Some(From), aggregate.actions ++ Map(From -> Nil))
+      case Where => ActionsTuple(Some(Where), aggregate.actions ++ Map(Where -> Nil))
+      case x => aggregate.currentToken.map(token => {
+        val nextArgs = aggregate.actions(token) :+ x
+        val nextActions = aggregate.actions ++ Map(token -> nextArgs)
+
+        aggregate.copy(actions = nextActions)
+      }).get
+    }
+  }
+
+  private def getValues(token: Token, value: JValue) =
+    token match {
+      case t: AnyToken => walkDown(t.value.split("\\."), value)
+      case _ => Seq(None)
+    }
+
+  private def walkDown(path: Seq[String], json: JValue): Seq[Option[JValue]] =
+    if (path.isEmpty) Seq(None)
+    else json match {
+      case JObject(obj) =>
+        obj.find(_.name == path.head).map(_.value) match {
+          case Some(value) if path.tail.isEmpty => Seq(Some(value))
+          case Some(value) => walkDown(path.tail, value)
+          case _ => Seq(None)
+        }
+      case JArray(arr) if path.tail.isEmpty => Seq(Some(JArray(arr)))
+      case JArray(arr) => arr.flatMap(v => walkDown(path.tail, v))
+      case _ => Seq(Some(json))
+    }
+
+  private case class ActionsTuple(currentToken: Option[Token],
+                                  actions: Map[Token, Seq[Token]])
+}
