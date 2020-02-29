f213c66e50b34e7d4f336e044e1e4a497ebf0f1b
mmalek06@gmail.com <mmalek06@gmail.com>
2020-02-29 15:47:12 +0100

    wildcards handling

--- src/test/scala/com/mmalek/jsonSql/FilteringTests.scala
+++ src/test/scala/com/mmalek/jsonSql/FilteringTests.scala
@@ -1,6 +1,6 @@
 package com.mmalek.jsonSql
 
-import com.mmalek.jsonSql.jsonParsing.dataStructures.{JNumber, JString}
+import com.mmalek.jsonSql.jsonParsing.dataStructures.{JField, JNumber, JObject, JString}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -21,6 +21,14 @@ class FilteringTests extends AnyFlatSpec with Matchers {
     error should be ("Filtering expressions should all relate to one object group. Check the github docs on how to properly select items from more than one group.")
   }
 
+  it should "not select anything from a simple object when the where clause statement contains the wildcard symbol" in {
+    val json = SampleJson.single
+    val query = """SELECT "items.id" FROM ##json## WHERE "items.*" = 1"""
+    val Right(result) = runQuery(query, json)
+
+    result("items.id") should be (List(None))
+  }
+
   it should "not select anything from a simple object when conditions are not met" in {
     val json = SampleJson.single
     val query = """SELECT "id", "age", AVG("age") AS "avgage", "fullname" FROM ##json## WHERE "id" = 0"""
@@ -39,24 +47,47 @@ class FilteringTests extends AnyFlatSpec with Matchers {
     result("fullname") should be (List(Some(JString("Raymond Mann"))))
   }
 
-  it should "filter the input properly when a flat where clause is passed" in {
+  it should "select two fields when using the wildcard symbol and when conditions are met" in {
     val json = SampleJson.list
-    val query = s"""
-        SELECT "items.id" AS "id", AVG("items.age") AS "itemsage", AVG("items.id") AS "itemsid", (2 + 3) * (4 + 1) AS "something"
+    val query =
+      """
+        SELECT "items.id" AS "id", "items.address.*" AS "address"
         FROM ##json##
-        WHERE "items.age" = 15 AND "items.address.city" = 'Gdańsk' OR "items.isEmployee" = false
+        WHERE "items.age" = 15
         """
     val Right(result) = runQuery(query, json)
 
-    result("id") should be (List(None, None, None, None, Some(JNumber(5)), None, None, None, None, Some(JNumber(10))))
-    result("itemsage") should be (List(Some(JNumber(3.4))))
-    result("itemsid") should be (List(Some(JNumber(1.5))))
+    result("id") should be (Seq(None, None, None, None, None, None, Some(JNumber(7)), None, None, None))
+    result("address") should be (
+      Seq(None, None, None, None, None, None,
+        Some(
+          JObject(
+            Seq(
+              JField("street", JString("Street3")),
+              JField("city", JString("City1"))))),
+        None, None, None))
+  }
+
+  it should "filter the input properly when a flat where clause is passed" in {
+    val json = SampleJson.list
+    val query =
+      """
+        SELECT "items.id" AS "id", AVG("items.age") AS "itemsage", AVG("items.id") AS "itemsid", (2 + 3) * (4 + 1) AS "something"
+        FROM ##json##
+        WHERE "items.age" = 15 AND "items.address.city" = 'City1' OR "items.isEmployee" = false
+        """
+    val Right(result) = runQuery(query, json)
+
+    result("id") should be (List(None, None, None, None, Some(JNumber(5)), None, Some(JNumber(7)), None, None, Some(JNumber(10))))
+    result("itemsage") should be (List(Some(JNumber(4.9))))
+    result("itemsid") should be (List(Some(JNumber(2.2))))
     result("something") should be (List(Some(JNumber(25))))
   }
 
   it should "filter the input properly when a bracketed where clause is passed" in {
     val json = SampleJson.list
-    val query = s"""
+    val query =
+      """
         SELECT "items.id" AS "id"
         FROM ##json##
         WHERE ("items.age" > 10 AND "items.age" < 20 AND "items.address.city" = 'City1') OR "items.isEmployee" = false
@@ -68,7 +99,8 @@ class FilteringTests extends AnyFlatSpec with Matchers {
 
   it should "filter the input properly, based on where clause that's comparing two properties of the same object" in {
     val json = SampleJson.list
-    val query = s"""
+    val query =
+      """
         SELECT "items.id" AS "id"
         FROM ##json##
         WHERE "items.age" = "items.id"
--- src/test/scala/com/mmalek/jsonSql/SampleJson.scala
+++ src/test/scala/com/mmalek/jsonSql/SampleJson.scala
@@ -126,5 +126,4 @@ object SampleJson {
           }
       }
       """
-
 }
